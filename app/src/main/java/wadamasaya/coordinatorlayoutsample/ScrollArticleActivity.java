18b851bad0ba2a7abcdf3669e778c093052d6ba3
wadamasaya <16010418@anabuki-college.ac.jp>
2018-08-01 21:00:32 +0900

    init

---
+++ app/src/main/java/wadamasaya/coordinatorlayoutsample/ScrollArticleActivity.java
@@ -0,0 +1,27 @@
+package wadamasaya.coordinatorlayoutsample;
+
+import android.graphics.Color;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class ScrollArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scroll_article);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolbarLayout = findViewById(R.id.toolbarLayout);
+        toolbarLayout.setTitle(getString(R.string.toolbar_title));
+        toolbarLayout.setExpandedTitleColor(Color.WHITE);
+        toolbarLayout.setCollapsedTitleTextColor(Color.LTGRAY);
+
+    }
+
+
+}
